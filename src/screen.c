1de01c70ea03e9c5c5b2cd38c9474d2938cba046
chernandezba <chernandezba@hotmail.com>
2020-04-08 18:42:30 +0200

    Add solarized colour table

--- src/screen.c
+++ src/screen.c
@@ -327,6 +327,47 @@ const int z88_colortable_original[4]={
 //ver screen.h, Z88_PXCOLON, etc
 
 
+//Tabla con colores para tema de GUI Solarized. 
+/*
+SOLARIZED HEX     16/8 TERMCOL  XTERM/HEX   L*A*B      RGB         HSB
+--------- ------- ---- -------  ----------- ---------- ----------- -----------
+base03    #002b36  8/4 brblack  234 #1c1c1c 15 -12 -12   0  43  54 193 100  21
+base02    #073642  0/4 black    235 #262626 20 -12 -12   7  54  66 192  90  26
+base01    #586e75 10/7 brgreen  240 #585858 45 -07 -07  88 110 117 194  25  46
+base00    #657b83 11/7 bryellow 241 #626262 50 -07 -07 101 123 131 195  23  51
+base0     #839496 12/6 brblue   244 #808080 60 -06 -03 131 148 150 186  13  59
+base1     #93a1a1 14/4 brcyan   245 #8a8a8a 65 -05 -02 147 161 161 180   9  63
+base2     #eee8d5  7/7 white    254 #e4e4e4 92 -00  10 238 232 213  44  11  93
+base3     #fdf6e3 15/7 brwhite  230 #ffffd7 97  00  10 253 246 227  44  10  99
+yellow    #b58900  3/3 yellow   136 #af8700 60  10  65 181 137   0  45 100  71
+orange    #cb4b16  9/3 brred    166 #d75f00 50  50  55 203  75  22  18  89  80
+red       #dc322f  1/1 red      160 #d70000 50  65  45 220  50  47   1  79  86
+magenta   #d33682  5/5 magenta  125 #af005f 50  65 -05 211  54 130 331  74  83
+violet    #6c71c4 13/5 brmagenta 61 #5f5faf 50  15 -45 108 113 196 237  45  77
+blue      #268bd2  4/4 blue      33 #0087ff 55 -10 -45  38 139 210 205  82  82
+cyan      #2aa198  6/6 cyan      37 #00afaf 60 -35 -05  42 161 152 175  74  63
+green     #859900  2/2 green     64 #5f8700 60 -20  65 133 153   0  68 100  60
+*/
+const int solarized_colortable_original[16]={
+0x002b36, //base03
+0x073642, //base02
+0x586e75, //base01
+0x657b83, //base00
+0x839496, //base0
+0x93a1a1, //base1   (5)
+0xeee8d5, //base2
+0xfdf6e3, //base3
+0xb58900, //yellow
+0xcb4b16, //orange
+0xdc322f, //red     (10)
+0xd33682, //magenta
+0x6c71c4, //violet
+0x268bd2, //blue
+0x2aa198, //cyan
+0x859900, //green    (15)
+};
+
+
 //Tabla con los colores extra del Spectra.
 //Valores para intensidades de color:
 /*
@@ -8312,6 +8353,11 @@ G  G   R   R   B   B
 				screen_set_colour_normal(HEATMAP_INDEX_FIRST_COLOR+i,(r<<16)|(g<<8)|b);
 			}
 
+				//Colores Solarized. No los pasamos a grises estos
+				for (i=0;i<SOLARIZED_TOTAL_PALETTE_COLOURS;i++) {
+					screen_set_colour_normal(SOLARIZED_INDEX_FIRST_COLOR+i,solarized_colortable_original[i]);
+				}			
+
 
 
 		}
@@ -8483,6 +8529,11 @@ Bit 6 GRN1 most  significant bit of green.
 					int colorheat=i<<16;
 					debug_printf (VERBOSE_PARANOID,"Heatmap color: %02XH 32 bit: %06XH",i,colorheat);
 					screen_set_colour_normal(HEATMAP_INDEX_FIRST_COLOR+i,colorheat);
+				}
+
+				//Colores Solarized
+				for (i=0;i<SOLARIZED_TOTAL_PALETTE_COLOURS;i++) {
+					screen_set_colour_normal(SOLARIZED_INDEX_FIRST_COLOR+i,solarized_colortable_original[i]);
 				}
 
 
