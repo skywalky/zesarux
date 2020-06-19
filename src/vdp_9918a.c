24d9413f03e4d368a93950f8d41e28a2458cd020
chernandezba <chernandezba@hotmail.com>
2020-06-20 00:23:08 +0200

    mc

--- src/vdp_9918a.c
+++ src/vdp_9918a.c
@@ -609,8 +609,8 @@ void vdp_9918a_render_sprites_no_rainbow(z80_byte *vram)
 
 
 
-    //En modos 1 y 2 permitimos sprites????
-    //if (video_mode!=1 && video_mode!=2) return;
+    //Solo En modos 1 y 2 permitimos sprites? Pitfall1 pone mode 32x24 y usa sprites??
+    if (video_mode!=1 && video_mode!=2) return;
 
 
 
@@ -1580,8 +1580,8 @@ void vdp_9918a_render_rainbow_sprites_line(int scanline,z80_int *scanline_buffer
 
 
 
-    //En modos 1 y 2 permitimos sprites. ???
-    //if (video_mode!=1 && video_mode!=2) return;
+    //Solo En modos 1 y 2 permitimos sprites? Pitfall1 pone mode 32x24 y usa sprites??
+    if (video_mode!=1 && video_mode!=2) return;
 
 
 
