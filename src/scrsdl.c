2af373ea743dc417e93aacfe9bd71b96526ae5cc
chernandezba <chernandezba@hotmail.com>
2020-07-02 17:28:40 +0200

    mc

--- src/scrsdl.c
+++ src/scrsdl.c
@@ -933,7 +933,7 @@ void scrsdl_deal_keys(int pressrelease,int tecla)
 
 
         int tecla_gestionada_sam_ql=0;
-        if (MACHINE_IS_SAM || MACHINE_IS_QL || MACHINE_IS_MSX || MACHINE_IS_SVI) {
+        if (MACHINE_IS_SAM || MACHINE_IS_QL || MACHINE_IS_MSX || MACHINE_IS_SVI) {
                 tecla_gestionada_sam_ql=1;
 
                         if (tecla==scrsdl_keymap_z88_cpc_minus) util_set_reset_key_common_keymap(UTIL_KEY_COMMON_KEYMAP_MINUS,pressrelease);
