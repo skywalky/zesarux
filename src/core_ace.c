819388582e7d5680971c2b55d8eb12c571b917fd
chernandezba <chernandezba@hotmail.com>
2020-10-05 19:31:01 +0200

    add last core frame elapsed time for jupiter ace

--- src/core_ace.c
+++ src/core_ace.c
@@ -230,6 +230,9 @@ void cpu_core_loop_ace(void)
 
 				t_scanline=0;
 
+
+				timer_get_elapsed_core_frame_post();
+
                                 //Parche para maquinas que no generan 312 lineas, porque si enviamos menos sonido se escuchara un click al final
                                 //Es necesario que cada frame de pantalla contenga 312 bytes de sonido
 				//Igualmente en la rutina de envio_audio se vuelve a comprobar que todo el sonido a enviar 
@@ -316,6 +319,9 @@ void cpu_core_loop_ace(void)
                         esperando_tiempo_final_t_estados.v=0;
                         interlaced_numero_frame++;
                         //printf ("%d\n",interlaced_numero_frame);
+
+					//Para calcular lo que se tarda en ejecutar todo un frame
+					timer_get_elapsed_core_frame_pre();						
                 }
 
 
