e7a5ce0d0d0080d7e3bb33dc4113b608091a8374
chernandezba <chernandezba@hotmail.com>
2020-10-24 00:28:36 +0200

    code cleaning

--- src/core_ql.c
+++ src/core_ql.c
@@ -50,18 +50,19 @@
 #include "settings.h"
 #include "ql_i8049.h"
 #include "ql_qdos_handler.h"
-
+#include "ql_zx8302.h"
 
 
 
 z80_byte byte_leido_core_ql;
-char buffer_disassemble[100000];
 
-extern unsigned char ql_pc_intr;
+//extern unsigned char ql_pc_intr;
 
-int refresca=0;
+//int refresca=0;
 
-int temporal_parpadeo_ql;
+//char buffer_disassemble[100000];
+
+//int temporal_parpadeo_ql;
 
 /*
 void ql_chapuza_parpadeo_cursor(void)
@@ -100,10 +101,10 @@ void ql_chapuza_parpadeo_cursor(void)
 void cpu_core_loop_ql(void)
 {
 
-                debug_get_t_stados_parcial_pre();
+    debug_get_t_stados_parcial_pre();
 
 
-		timer_check_interrupt();
+    timer_check_interrupt();
 
 //#ifdef COMPILE_STDOUT
 //              if (screen_stdout_driver) scr_stdout_printchar();
@@ -112,18 +113,12 @@ void cpu_core_loop_ql(void)
 //#ifdef COMPILE_SIMPLETEXT
 //                if (screen_simpletext_driver) scr_simpletext_printchar();
 //#endif
-                if (chardetect_detect_char_enabled.v) chardetect_detect_char();
-                if (chardetect_printchar_enabled.v) chardetect_printchar();
+    if (chardetect_detect_char_enabled.v) chardetect_detect_char();
+    if (chardetect_printchar_enabled.v) chardetect_printchar();
 
 
 
-                //temp condicion
-                //if (ql_microdrive_floppy_emulation) 
-                  ql_rom_traps();
-
-
-
-
+    ql_rom_traps();
 
 
 
@@ -138,25 +133,11 @@ void cpu_core_loop_ql(void)
 
 
 #ifdef EMULATE_CPU_STATS
-                                util_stats_increment_counter(stats_codsinpr,byte_leido_core_ql);
+                util_stats_increment_counter(stats_codsinpr,byte_leido_core_ql);
 #endif
 
 
 
-        /*char buffer_disassemble[1000];
-        unsigned int pc_ql=100;
-
-        // Access the internals of the CPU
-        pc_ql=m68k_get_reg(NULL, M68K_REG_PC);
-
-        int longitud=m68k_disassemble(buffer_disassemble, pc_ql, M68K_CPU_TYPE_68000);
-        printf ("%08XH %d %s\n",pc_ql,longitud,buffer_disassemble);
-        */
-
-
-	//intento de detectar cuando se llama a rutina rom de mt.ipcom
-	//unsigned int pc_ql=get_ql_pc();
-	//if (pc_ql==0x0872) exit(1);
 
 	//Ejecutar opcode
 #ifdef EMULATE_VISUALMEM
@@ -197,14 +178,6 @@ void cpu_core_loop_ql(void)
                 m68k_execute(1);
 
 
-		/* Disasemble one instruction at pc and store in str_buff */
-//unsigned int m68k_disassemble(char* str_buff, unsigned int pc, unsigned int cpu_type)
-//unsigned int m68k_get_reg(void* context, m68k_register_t regnum)
-//                case M68K_REG_PC:       return MASK_OUT_ABOVE_32(cpu->pc);
-//		unsigned int registropcql=m68k_get_reg(NULL,M68K_REG_PC);
-//		printf ("Registro PC: %08XH\n",registropcql);
-//		m68k_disassemble(buffer_disassemble,registropcql,M68K_CPU_TYPE_68000);
-
 
 				//Simplemente incrementamos los t-estados un valor inventado, aunque luego al final parece ser parecido a la realidad
 				t_estados+=4;
@@ -227,40 +200,30 @@ void cpu_core_loop_ql(void)
 
 
 
-                        //Envio sonido
+            //Envio sonido
 
-                        audio_valor_enviar_sonido=0;
+            audio_valor_enviar_sonido=0;
 
-                        //Usando simulacion por AY
-                        //audio_valor_enviar_sonido +=da_output_ay();
+            //Usando simulacion por AY
+            //audio_valor_enviar_sonido +=da_output_ay();
 
-                        //Usando emulacion del chip intel 
-                        audio_valor_enviar_sonido +=ql_audio_da_output();
-
-
-                        if (realtape_inserted.v && realtape_playing.v) {
-                                realtape_get_byte();
-                                //audio_valor_enviar_sonido += realtape_last_value;
-				if (realtape_loading_sound.v) {
-				audio_valor_enviar_sonido /=2;
-                                audio_valor_enviar_sonido += realtape_last_value/2;
-                                //Sonido alterado cuando top speed
-                                if (timer_condicion_top_speed() ) audio_valor_enviar_sonido=audio_change_top_speed_sound(audio_valor_enviar_sonido);
-				}
-                        }
-
-                        //Ajustar volumen
-                        if (audiovolume!=100) {
-                                audio_valor_enviar_sonido=audio_adjust_volume(audio_valor_enviar_sonido);
-                        }
+            //Usando emulacion del chip intel 
+            audio_valor_enviar_sonido +=ql_audio_da_output();
 
 
-                        audio_send_mono_sample(audio_valor_enviar_sonido);
+
+            //Ajustar volumen
+            if (audiovolume!=100) {
+                    audio_valor_enviar_sonido=audio_adjust_volume(audio_valor_enviar_sonido);
+            }
 
 
-                        //ay_chip_siguiente_ciclo();
+            audio_send_mono_sample(audio_valor_enviar_sonido);
 
-                        ql_audio_next_cycle();
+
+            
+
+            ql_audio_next_cycle();
 
 			//se supone que hemos ejecutado todas las instrucciones posibles de toda la pantalla. refrescar pantalla y
 			//esperar para ver si se ha generado una interrupcion 1/50
@@ -274,17 +237,17 @@ void cpu_core_loop_ql(void)
 				timer_get_elapsed_core_frame_post();
 
 
-                                //Parche para maquinas que no generan 312 lineas, porque si enviamos menos sonido se escuchara un click al final
-                                //Es necesario que cada frame de pantalla contenga 312 bytes de sonido
-				//Igualmente en la rutina de envio_audio se vuelve a comprobar que todo el sonido a enviar
-				//este completo; esto es necesario para Z88
+                //Parche para maquinas que no generan 312 lineas, porque si enviamos menos sonido se escuchara un click al final
+                //Es necesario que cada frame de pantalla contenga 312 bytes de sonido
+                //Igualmente en la rutina de envio_audio se vuelve a comprobar que todo el sonido a enviar
+                //este completo; esto es necesario para Z88
 
-                                int linea_estados=t_estados/screen_testados_linea;
+                int linea_estados=t_estados/screen_testados_linea;
 
-                                while (linea_estados<312) {
-                                        audio_send_mono_sample(audio_valor_enviar_sonido);
-                                        linea_estados++;
-                                }
+                while (linea_estados<312) {
+                        audio_send_mono_sample(audio_valor_enviar_sonido);
+                        linea_estados++;
+                }
 
 
 				t_estados -=screen_testados_total;
@@ -296,12 +259,12 @@ void cpu_core_loop_ql(void)
 
 				siguiente_frame_pantalla();
 
-        contador_parpadeo--;
-            //printf ("Parpadeo: %d estado: %d\n",contador_parpadeo,estado_parpadeo.v);
-            if (!contador_parpadeo) {
-                    contador_parpadeo=20; //TODO no se si esta es la frecuencia normal de parpadeo
-                    estado_parpadeo.v ^=1;
-            }
+                contador_parpadeo--;
+                //printf ("Parpadeo: %d estado: %d\n",contador_parpadeo,estado_parpadeo.v);
+                if (!contador_parpadeo) {
+                        contador_parpadeo=20; //TODO no se si esta es la frecuencia normal de parpadeo
+                        estado_parpadeo.v ^=1;
+                }
 
 
 				if (debug_registers) scr_debug_registers();
@@ -319,16 +282,6 @@ void cpu_core_loop_ql(void)
 				}
 
 
-			//Prueba generar interrupcion
-			//printf ("Generar interrupcion\n");
-			 //m68k_set_irq(0); de 0 a 6 parece no tener efecto. poniendo 7 no pasa el test de ram
-			/*m68k_set_irq(1);
-			m68k_set_irq(2);
-			m68k_set_irq(3);
-			m68k_set_irq(4);
-			m68k_set_irq(5);
-			m68k_set_irq(6);*/
-
 
 /*
 * read addresses
@@ -344,24 +297,12 @@ pc_intr equ     $18021  bits 4..0 set as pending level 2 interrupts
             //frame. hace parpadear pantalla
             ql_pc_intr |=8;
 
-                        //No estoy seguro si esto son las interrupciones que genera el timer o no
-                        //Esto acaba generando llamadas a leer PC_INTR		Interrupt register
+            //No estoy seguro si esto son las interrupciones que genera el timer o no
+            //Esto acaba generando llamadas a leer PC_INTR		Interrupt register
 			m68k_set_irq(2);
 			
 
-                        //Chapuza para hacer parpadear el cursor
-                        //ql_chapuza_parpadeo_cursor();
-                        
-                       
-
-
-
-                                //Final de instrucciones ejecutadas en un frame de pantalla
-                                if (iff1.v==1) {
-                                        interrupcion_maskable_generada.v=1;
-                                }
-
-
+       
 
 			}
 
@@ -397,54 +338,13 @@ pc_intr equ     $18021  bits 4..0 set as pending level 2 interrupts
                         interlaced_numero_frame++;
                         //printf ("%d\n",interlaced_numero_frame);
 
-			//Para calcular lo que se tarda en ejecutar todo un frame
-			timer_get_elapsed_core_frame_pre();
+                        //Para calcular lo que se tarda en ejecutar todo un frame
+                        timer_get_elapsed_core_frame_pre();
                 }
 
 
 
-		//Interrupcion de cpu. gestion im0/1/2.
-		if (interrupcion_maskable_generada.v || interrupcion_non_maskable_generada.v) {
-
-			debug_fired_interrupt=1;
-
-                        //ver si esta en HALT
-                        if (z80_ejecutando_halt.v) {
-                                        z80_ejecutando_halt.v=0;
-                                        reg_pc++;
-
-                        }
-
-			if (1==1) {
-
-					if (interrupcion_non_maskable_generada.v) {
-						debug_anota_retorno_step_nmi();
-						interrupcion_non_maskable_generada.v=0;
-
-
-
-					}
-
-
-					if (1==1) {
-
-
-					//justo despues de EI no debe generar interrupcion
-					//e interrupcion nmi tiene prioridad
-						if (interrupcion_maskable_generada.v && byte_leido_core_ql!=251) {
-						debug_anota_retorno_step_maskable();
-						//Tratar interrupciones maskable
-
-
-
-
-					}
-				}
-
-
-			}
-
-  }
+	
                 debug_get_t_stados_parcial_post();
 
 
