edcec05a8bdab889992fa951ceed447ea0d73031
chernandezba <chernandezba@hotmail.com>
2020-11-23 20:36:43 +0100

    more tests for the final version

--- src/testsound.sh
+++ src/testsound.sh
@@ -6,17 +6,17 @@ for DRIVER in pulse alsa sdl dsp null; do
 echo "Driver $DRIVER"
 sleep 5
 
-./zesarux --noconfigfile --verbose 3 --machine 128k extras/media/spectrum/music.tap --ao $DRIVER
-./zesarux --noconfigfile --verbose 3 --machine 128k extras/media/spectrum/beeper/Digi_Pop.tap --ao $DRIVER
+./zesarux --noconfigfile --enable-silencedetector --verbose 3 --machine 128k extras/media/spectrum/music.tap --ao $DRIVER
+./zesarux --noconfigfile --enable-silencedetector --verbose 3 --machine 128k extras/media/spectrum/beeper/Digi_Pop.tap --ao $DRIVER
 
 echo "Testear silencio. Primero en 128k, beeper silence"
 echo "Luego pasar a 48k, saltaran los dos silence (porque no habra AY Chip)"
 sleep 3
 
-./zesarux --noconfigfile --verbose 3 --machine 128k --ao $DRIVER
-./zesarux --noconfigfile --verbose 3 extras/media/zx81/aydemo.p --ao $DRIVER
-./zesarux --noconfigfile --verbose 3 extras/media/zx81/ORQUESTA.P --ao $DRIVER
-./zesarux --noconfigfile --verbose 3 extras/media/z88/lem.epr --ao $DRIVER
-./zesarux --noconfigfile --verbose 3 extras/snap_tests/test3200z88.zx --ao $DRIVER
+./zesarux --noconfigfile --enable-silencedetector --verbose 3 --machine 128k --ao $DRIVER
+./zesarux --noconfigfile --enable-silencedetector --verbose 3 extras/media/zx81/aydemo.p --ao $DRIVER
+./zesarux --noconfigfile --enable-silencedetector --verbose 3 extras/media/zx81/ORQUESTA.P --ao $DRIVER
+./zesarux --noconfigfile --enable-silencedetector --verbose 3 extras/media/z88/lem.epr --ao $DRIVER
+./zesarux --noconfigfile --enable-silencedetector --verbose 3 extras/snap_tests/test3200z88.zx --ao $DRIVER
 
 done
