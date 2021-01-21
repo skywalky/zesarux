4436a2f60b40c76dea126e4e07743ac2b791d315
chernandezba <chernandezba@hotmail.com>
2021-01-21 20:24:26 +0100

    mc

---
+++ src/other_things.c
@@ -0,0 +1,49 @@
+/*
+    ZEsarUX  ZX Second-Emulator And Released for UniX 
+    Copyright (C) 2013 Cesar Hernandez Bano
+
+    This file is part of ZEsarUX.
+
+    ZEsarUX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+/*
+
+Hi there! This is not really a source code file, this is an easter egg! 
+Congratulations!
+
+Actually this is a file which I list all the easter eggs in ZEsarUX, 
+so you can easily try them. 
+
+Please, keep the list secret and don't tell anyone,
+let's keep the magic :)
+
+So, the list:
+
+1. This file. This is a simple easter egg, but it is one, right? XD
+
+2. Using ZRCP, run the command "easter-egg". See what happens...
+
+3. Using ZRCP, run the command "ATDT". There's a funny answer to that
+
+4. Enable ZX Desktop. Go to the about menu, and you'll see something different... 
+The answer for that change, in the FAQ
+
+5. Disable ZX Desktop. Go to the about menu, press F key (used to find text). 
+Just press enter to the text to find. You'll see a cursor in the window,
+select the second line and press Enter...
+
+*/
