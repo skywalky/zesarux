9d6d28dacf770f94a985d7983c2ed76683ca077d
chernandezba <chernandezba@hotmail.com>
2021-03-04 18:49:19 +0100

    mc

--- src/screen.c
+++ src/screen.c
@@ -2738,6 +2738,17 @@ int scr_putchar_menu_comun_zoom_reduce_charwidth(int bit)
 		saltar_pixeles_size5[2]=7;			
 	}
 
+    if (estilo_gui_activo==ESTILO_GUI_RETROMAC || estilo_gui_activo==ESTILO_GUI_TURBOVISION) {
+        saltar_pixeles_size7=7;
+
+        saltar_pixeles_size6[0]=6;
+        saltar_pixeles_size6[1]=7;
+
+        saltar_pixeles_size5[0]=0;
+        saltar_pixeles_size5[1]=6;
+        saltar_pixeles_size5[2]=7;
+    }
+
 
 	//Los demas se ajustan bien al escalado por defecto
 
