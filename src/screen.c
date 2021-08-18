cf709e7da138ac16687871a0e98c34f34fba560b
chernandezba <chernandezba@hotmail.com>
2021-08-18 18:33:44 +0200

    code cleaning

--- src/screen.c
+++ src/screen.c
@@ -2938,98 +2938,70 @@ void scr_putchar_menu_comun_zoom(z80_byte caracter,int x,int y,z80_bit inverse,i
 
 void scr_putchar_footer_comun_zoom(z80_byte caracter,int x,int y,z80_bit inverse,int tinta,int papel)
 {
-	        int color;
-        z80_byte bit;
-        z80_byte line;
-        z80_byte byte_leido;
+    int color;
+    z80_byte bit;
+    z80_byte line;
+    z80_byte byte_leido;
 
-        //printf ("tinta %d papel %d\n",tinta,papel);
+    //printf ("tinta %d papel %d\n",tinta,papel);
 
-        //margenes de zona interior de pantalla. Para modo rainbow
-        int margenx_izq;
-        int margeny_arr;
+    //margenes de zona interior de pantalla. Para modo rainbow
+    int margenx_izq;
+    int margeny_arr;
 
 	int zoom_level=1;
 
 	z80_byte *puntero;
 	puntero=&char_set[(caracter-32)*8];
 
-        scr_return_margenxy_rainbow(&margenx_izq,&margeny_arr);
+    scr_return_margenxy_rainbow(&margenx_izq,&margeny_arr);
 
-        //Caso de pentagon y en footer
-        if (pentagon_timing.v && y>=31) margeny_arr=56*border_enabled.v;
+    //Caso de pentagon y en footer
+    if (pentagon_timing.v && y>=31) margeny_arr=56*border_enabled.v;
 
-        y=y*8;
+    y=y*8;
 
-        for (line=0;line<8;line++,y++) {
-          byte_leido=*puntero++;
-          if (inverse.v==1) byte_leido = byte_leido ^255;
-          for (bit=0;bit<8;bit++) {
-                if (byte_leido & 128 ) color=tinta;
-                else color=papel;
-
-
+    for (line=0;line<8;line++,y++) {
+        byte_leido=*puntero++;
+        if (inverse.v==1) byte_leido = byte_leido ^255;
+        for (bit=0;bit<8;bit++) {
+            if (byte_leido & 128 ) color=tinta;
+            else color=papel;
 
 
-                byte_leido=(byte_leido&127)<<1;
+            byte_leido=(byte_leido&127)<<1;
 
-                //este scr_putpixel_zoom_rainbow tiene en cuenta los timings de la maquina (borde superior, por ejemplo)
+            //este scr_putpixel_zoom_rainbow tiene en cuenta los timings de la maquina (borde superior, por ejemplo)
 
-                int xfinal,yfinal;
+            int xfinal,yfinal;
 
-                if (rainbow_enabled.v==1) {
-                        //xfinal=(((x*8)+bit)*zoom_level);
-                        xfinal=(((x*8)+bit)*zoom_level);
-                        xfinal +=margenx_izq;
+            if (rainbow_enabled.v==1) {
+                    //xfinal=(((x*8)+bit)*zoom_level);
+                    xfinal=(((x*8)+bit)*zoom_level);
+                    xfinal +=margenx_izq;
 
-                        yfinal=y*zoom_level;
-                        yfinal +=margeny_arr;
-                }
+                    yfinal=y*zoom_level;
+                    yfinal +=margeny_arr;
+            }
 
-                else {
-                        //xfinal=((x*8)+bit)*zoom_level;
-                        xfinal=((x*8)+bit)*zoom_level;
-                        yfinal=y*zoom_level;
-                }
-
-
- //Hacer zoom de ese pixel si conviene
+            else {
+                    //xfinal=((x*8)+bit)*zoom_level;
+                    xfinal=((x*8)+bit)*zoom_level;
+                    yfinal=y*zoom_level;
+            }
 
 
-                //Ancho de caracter 8, 7 y 6 pixeles
-                /*if (menu_char_width==8) scr_putpixel_gui_zoom(xfinal,yfinal,color,zoom_level);
-
-                //Si 7, saltar primer pixel a la izquierda
-                else if (menu_char_width==7) {
-                        if (bit!=0) scr_putpixel_gui_zoom(xfinal,yfinal,color,zoom_level);
-                }
-
-                //Si 6, saltar dos pixeles: primero izquierda y primero derecha
-                else if (menu_char_width==6) {
-                        if (bit!=0 && bit!=7) scr_putpixel_gui_zoom(xfinal,yfinal,color,zoom_level);
-                }
-
-                //Si 5, saltar tres pixeles: primero izquierda y centro y primero derecha
-                else if (menu_char_width==5) {
-                        if (bit!=0 && bit!=6 && bit!=7) scr_putpixel_gui_zoom(xfinal,yfinal,color,zoom_level);
-                }*/
-
-	                                if (rainbow_enabled.v==1) scr_putpixel_zoom_rainbow(xfinal,yfinal,color);
-
-                                else scr_putpixel_zoom(xfinal,yfinal,color);							
-
-                /*int incx,incy;
-                for (incy=0;incy<zoom_level;incy++) {
-                        for (incx=0;incx<zoom_level;incx++) {
-                                if (rainbow_enabled.v==1) scr_putpixel_zoom_rainbow(xfinal+incx,yfinal+incy,color);
-
-                                else scr_putpixel_zoom(xfinal+incx,yfinal+incy,color);
-                        }
-                }*/
+            //Hacer zoom de ese pixel si conviene
 
 
-           }
+            if (rainbow_enabled.v==1) scr_putpixel_zoom_rainbow(xfinal,yfinal,color);
+
+            else scr_putpixel_zoom(xfinal,yfinal,color);							
+
+
+
         }
+    }
 }								
 
 
